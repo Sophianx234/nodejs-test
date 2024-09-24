36c7dfa241b8986cf54d21cbb63fbb463969f899
Damian X <dx4336969@gmail.com>
2024-09-24 08:03:34 +0100

    building a very simple api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const http = require('http');
+const url = require('url');
+
 
 /*
 const textIn = fs.readFileSync('./txt/input.txt','utf-8')
@@ -31,10 +33,28 @@ console.log('Will read file!') */
 
 
 // Server
+const data  = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8',)
+const dataObj = JSON.parse(data)
 
 const server =http.createServer((req, res)=>{
-    console.log(res)
-    res.end("Hello from the server")
+    const pathName = req.url
+    if(pathName === '/' || pathName=== '/overview')
+        {
+
+            res.end('This is the Overview')
+        } 
+    else if(pathName === '/product'){
+
+        res.end('This is the Product ')
+    }
+    else if(pathName === '/api'){
+        res.end(data)
+        
+    }
+    else {
+        res.end('Page not Found!')
+    }
+    
 }) 
 
 server.listen(8000,'127.0.0.1',()=>{
