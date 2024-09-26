5a4f37ef12b88b00de14cbfe26c75fbcd7659c86
Damian X <dx4336969@gmail.com>
2024-09-26 11:19:58 +0000

    fixing bug ;

--- index.js
+++ index.js
@@ -1,67 +1,55 @@
+
+const http = require('http')
+const path = require('path')
 const fs = require('fs')
-const http = require('http');
-const url = require('url');
 
 
-/*
-const textIn = fs.readFileSync('./txt/input.txt','utf-8')
+const replaceTemplate =(temp,product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName)
+          output = output.replace(/{%DESCRIPTION%}/g,product.description)
+          output = output.replace(/{%IMAGE%}/g,product.image)
+          output = output.replace(/{%QUANTITY%}/g,product.price)
+          output = output.replace(/{%FROM%}/g,product.from)
+          output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
+          output = output.replace(/{%ORGANIC%}/g,product.nutrients)
+          return output
+}
 
-console.log(textIn)
-
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
-fs.writeFileSync('./txt/output.txt',textOut)
-console.log('file Written')
- */
-
-//Non-blocking, asynchronous
-/* 
-fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
-    
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
-    console.log(data2)
-    fs.readFile(`./txt/append.txt`,'utf-8',(err,data3)=>{
-    console.log(data3)
-    fs.writeFile('./txt/final.txt', `${data2}\n ${data3}`,'utf-8',err=>{
-         console.log('Your file has been written')
-    })
-    })
-    })
-    
-})
-
-console.log('Will read file!') */
-
-
-// Server
-const data  = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8',)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
 const dataObj = JSON.parse(data)
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempProducts = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
-const server =http.createServer((req, res)=>{
+const server = http.createServer((req,res)=>{
     const pathName = req.url
 
-    // overview page
-    if(pathName === '/' || pathName=== '/overview')
-        {
-
-            res.end('This is the Overview')
-        } 
-        // product page
-    else if(pathName === '/product'){
-
-        res.end('This is the Product ')
-    }
-    // api page
-    else if(pathName === '/api'){
-        res.end(data)
-        
-    }
-    // not Found page
-    else {
-        res.end('Page not Found!')
-    }
+    if(pathName === '/' || pathName === '/overview'){
+        res.writeHead(200,{
+            'content-type': 'text/html'
+        })
+        const cardsHtml = dataObj.map(el=>replaceTemplate(tempCard,el)).join('')
+        const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g,cardsHtml)
+        res.end(output)
     
-}) 
+}
+    else if(pathName === '/api'){
+        res.writeHead(200,{
+            'content-type': 'application/json'
+    })
+            res.end(data)
+}
+    else{
+        res.writeHead(404,{
+            'content-type': 'text/html',
+            'my-own-header': 'Page not Found'
+        })
+        res.end('Page not Found')
+}
 
-server.listen(8000,'127.0.0.1',()=>{
-    console.log('listening to request from port 800');
+}
+)
+
+server.listen(8000,()=>{
+    console.log('listening to port 8000')
 })
